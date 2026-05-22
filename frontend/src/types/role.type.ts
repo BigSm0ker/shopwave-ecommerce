2d33bb36240fa8ae88decd57275917daa754111b
BigSmoke <cesarcadiz26@gmail.com>
2026-05-22 18:05:39 -0400

    ejemplo

--- frontend/src/types/role.type.ts
+++ frontend/src/types/role.type.ts
@@ -2,3 +2,13 @@ export type Role = "ROLE_USER" | "ROLE_ADMIN";
 
 export const USER_ROLE: Role = "ROLE_USER";
 export const ADMIN_ROLE: Role = "ROLE_ADMIN";
+
+
+
+// export type Role = "ROLE_USER" | "ROLE_ADMIN";
+
+// export const USER_ROLE: Role = "ROLE_USER";
+// export const ADMIN_ROLE: Role = "ROLE_ADMIN";
+
+
+
